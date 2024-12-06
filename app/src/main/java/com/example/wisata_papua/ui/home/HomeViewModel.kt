9f811c988945d325b9563e1ceef673d3a03730c0
haris2303 <hrsccf102@gmail.com>
2024-12-07 06:08:35 +0700

    first

---
+++ app/src/main/java/com/example/wisata_papua/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.wisata_papua.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
